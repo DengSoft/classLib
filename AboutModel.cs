45ccd892371123256dfdb68f70b43db2048ea01d
DengSoft <domphyone@gmail.com>
2016-05-20 15:04:34 +0800

    关于我们
    我们实体类

---
+++ AboutModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using DFS.Core.External.Contracts.Dto.App;
+
+namespace MobileApp.ResModels
+{
+    public class AboutModel:BaseResModel
+    {
+        /// <summary>
+        /// 关于大丰收的信息数据集合
+        /// </summary>
+        public List<AboutDto> AboutList
+        {
+            get; 
+            set;
+        }
+    }
+}
